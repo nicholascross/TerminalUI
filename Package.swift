31cb817b77fdf0f81b3e9bbc8e75d57465936bd0
Nicholas Cross <nicholas@nacrossweb.com>
2025-07-27 10:38:11 +1000

    Raise min deployment version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "TerminalUI",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v11),
     ],
     products: [
         .library(
